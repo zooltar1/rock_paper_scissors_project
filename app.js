d306b9a69a062f2ea5165b887754aa1293b22a94
Rafal Otka <zooltar1@gmail.com>
2023-04-10 22:01:16 +0200

    Add game function to app.js to play rounds based on gameCounter variable, add win counters for plaer and computer

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 let gameChoices = ['rock', 'paper', 'scissors'];
 let computerChoice;
 let playerChoice;
+let playerWin = 0;
+let computerWin = 0;
 
 // function that returns random number based on array length
 
@@ -13,30 +15,39 @@ function getComputerChoice () {
     return parseInt(randomChoice);
 }
 
-// asign random choice from game choices to computerChoice variable
-
-computerChoice = gameChoices[getComputerChoice()];
-
-// asign user input to playerChoice variable, made it case insensitive
-
-playerChoice = prompt('Type: Rock, Paper or Scissors').toLowerCase();
-
-// main function that checks who wins
+// function that checks who wins
 
 function playRound (computerChoice, playerChoice) {
     
     let message;
 
+    computerChoice = gameChoices[getComputerChoice()];
+    playerChoice = prompt('Welcome to Rock, Paper, Scissors Game! Type: Rock, Paper or Scissors').toLowerCase();
+
     if (computerChoice == playerChoice) {
         message = `Tie!`;
     } 
     else if (computerChoice === 'rock' && playerChoice === 'scissors' || computerChoice === 'paper' && playerChoice === 'rock' || computerChoice === 'scissors' && playerChoice === 'paper') {
+        computerWin += 1;
         message = `You lost. ${computerChoice} beats ${playerChoice}!`;
     }
     else if (computerChoice === 'rock' && playerChoice === 'paper' || computerChoice === 'paper' && playerChoice === 'scissors' || computerChoice === 'scissors' && playerChoice === 'rock') {
+        playerWin += 1;
         message = `You win ! ${playerChoice} beats ${computerChoice}!`;
     }
+    console.log('Player win counter: ' + playerWin);
+    console.log('Computer win counter: ' + computerWin);
     console.log(message);
 }
 
-playRound(computerChoice, playerChoice);
+// function called game() to play a 5 round game that keeps score and reports a winner or loser at the end
+
+function game() {
+    let gameCounter = 5;
+
+    for (let i = 0; i < gameCounter; i++) {
+        playRound(computerChoice, playerChoice);
+    }
+}
+
+game();
