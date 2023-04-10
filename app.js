51996966a3cb09e4b3fcd6c9d47c1ee1f5c49491
Rafal Otka <zooltar1@gmail.com>
2023-04-10 18:21:21 +0200

    Add basic plan what to do in app,js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("test");
+// #Rock Paper Scissors Game#
+
+// create variable 'computerSelection' to store computer choice
+
+// create variable 'playerSelection' to store player choice, make it case insensitive
+
+// create function called 'getComputerChoice' that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+
+// create function 'playRound' that plays a single round of Rock Paper Scissors, return result that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+// Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
